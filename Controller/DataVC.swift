3d4f4399de6f91c384590c15e4ccb0edbdd045ef
Lawrence <lawrencehan94@gmail.com>
2017-10-13 12:38:02 -0700

    v1

---
+++ Controller/DataVC.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+class Company {
+var ticker: String
+var name: String
+var value: Double
+	
+init(_ tickerInput: String, _ nameInput: String, _ valueInput: Double) {
+ticker = tickerInput
+name = nameInput
+value = valueInput
+}
+}
+
+class CompanyCell: UITableViewCell {
+
+  @IBOutlet weak var tickerLabel: UILabel!
+  @IBOutlet weak var nameLabel: UILabel!
+  @IBOutlet weak var valueLabel: UILabel!
+
+  func configureCell(company: Company) {
+    tickerLabel.text = company.ticker
+    companyLabel.text = company.name
+    valueLabel.text = company.value
+  }
+
+}
